d5470281c9e80428545bdd4edf4c6a9228763b31
Nikolay Dimolarov <dimolarov@hotmail.com>
2017-11-23 11:44:08 +0100

    added basic implementation of vcf export

--- excel_to_vcard.py
+++ excel_to_vcard.py
@@ -5,12 +5,25 @@ def parse_excel():
 
 
 def create_vcard(contact):
-    print("Creating vCard for" + contact.name)
-    # output vcf file
-    return None
+    vc_begin = "begin:vcard\n"
+    vc_version = "version:4.0\n"
+    vc_name = f"n;charset=utf-8:{contact['last_name']};{contact['first_name']};;;\n"
+    vc_title = f"title;charset=utf-8:{contact['title']}\n"
+    vc_org = f"org;charset=utf-8:{contact['org']}\n"
+    vc_phone = f"tel;type=work,voice:{contact'phone'}\n"
+    vc_end = "end:vcard\n"
+
+    fo = open(f"{contact['last_name'].lower()}.vcf", "w")
+    fo.writelines([vc_begin, vc_version, vc_name, vc_title, vc_org, vc_phone, vc_end])
+    fo.close()
+
+    print(f"Created vCard for {contact['last_name']}")
 
 
 def excel_to_vcard():
-    contacts = parse_excel()
+    #contacts = parse_excel()
+    # mock with Forrest Gump
+    contacts = [{"last_name": "Gump", "first_name": "Forrest", "title": "Shrimp Man", "org": "Bubba Gump Shrimp Co.",
+                 "phone": "+49 170 5 25 25 25"}]
     for c in contacts:
         create_vcard(c)
