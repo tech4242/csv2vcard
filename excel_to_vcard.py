8f5723ed0d7e1f0e0545df12f4adbeb12dc024de
Nikolay Dimolarov <dimolarov@hotmail.com>
2017-11-23 13:24:00 +0100

    added phone, linkedin, address and made changes to the vcard syntax

--- excel_to_vcard.py
+++ excel_to_vcard.py
@@ -4,17 +4,23 @@ def parse_excel():
     return []
 
 
-def create_vcard(contact):
-    vc_begin = "begin:vcard\n"
-    vc_version = "version:4.0\n"
-    vc_name = f"n;charset=utf-8:{contact['last_name']};{contact['first_name']};;;\n"
-    vc_title = f"title;charset=utf-8:{contact['title']}\n"
-    vc_org = f"org;charset=utf-8:{contact['org']}\n"
-    vc_phone = f"tel;type=work,voice:{contact['phone']}\n"
-    vc_end = "end:vcard\n"
+def create_vcard(contact: dict):
+    """
+    The mappings used below are from https://www.w3.org/TR/vcard-rdf/#Mapping
+    """
+    vc_begin = "BEGIN:VCARD\n"
+    vc_version = "VERSION:4.0\n"
+    vc_name = f"N;charset=utf-8:{contact['last_name']};{contact['first_name']};;;\n"
+    vc_title = f"TITLE;charset=utf-8:{contact['title']}\n"
+    vc_org = f"ORG;charset=utf-8:{contact['org']}\n"
+    vc_phone = f"TEL;type=work,voice:{contact['phone']}\n"
+    vc_email = f"EMAIL;type=work:{contact['email']}\n"
+    vc_linkedin = f"URL;type=work:{contact['linkedin']}\n"
+    vc_address = f"ADR;type=work;charset=utf-8:{contact['address']['street']};{contact['address']['city']};{contact['address']['p_code']};{contact['address']['country']}\n"
+    vc_end = "END:VCARD\n"
 
-    fo = open(f"{contact['last_name'].lower()}.vcf", "w")
-    fo.writelines([vc_begin, vc_version, vc_name, vc_title, vc_org, vc_phone, vc_end])
+    fo = open(f"{contact['last_name'].lower()}_{contact['first_name'].lower()}.vcf", "w")
+    fo.writelines([vc_begin, vc_version, vc_name, vc_title, vc_org, vc_phone, vc_email, vc_linkedin, vc_address, vc_end])
     fo.close()
 
     print(f"Created vCard for {contact['last_name']}")
@@ -24,6 +30,9 @@ def excel_to_vcard():
     #contacts = parse_excel()
     # mock with Forrest Gump
     contacts = [{"last_name": "Gump", "first_name": "Forrest", "title": "Shrimp Man", "org": "Bubba Gump Shrimp Co.",
-                 "phone": "+49 170 5 25 25 25"}]
+                 "phone": "+49 170 5 25 25 25", "email": "forrestgump@example.com",
+                 "linkedin": "https://www.linkedin.com/in/forrestgump",
+                 "address": {"street": "42 Plantation St.", "city": "Baytown", "p_code": "30314",
+                             "country": "United States of America"}}]
     for c in contacts:
         create_vcard(c)
