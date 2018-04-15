ef420a544681f19b04cef2421820b9b4ed4fc41c
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-04-15 19:11:43 +0200

    0.2.1 for pypi

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 setup(
   name = 'csv2vcard',
   packages = ['csv2vcard'],
-  version = '0.2',
+  version = '0.2.1',
   description = 'A library for converting csvs to vCards',
   long_description = README,
   author = 'Nikolay Dimolarov',
   author_email = 'dimolarov@hotmail.com',
   url = 'https://github.com/tech4242/csv2vcard',
-  download_url = 'https://github.com/tech4242/csv2vcard/archive/0.2.tar.gz',
+  download_url = 'https://github.com/tech4242/csv2vcard/archive/0.2.1.tar.gz',
   keywords = ['csv', 'vcard', 'export'],
   python_requires = '>=3.6',
   classifiers = [
