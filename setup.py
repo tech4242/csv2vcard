8a6a7e191476a4ea1879f96a022543df73f23467
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-04-15 16:29:50 +0200

    Starting prep for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'csv-to-vcard',
+  packages = ['csv-to-vcard'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library for converting csvs to vCards',
+  author = 'Nikolay Dimolarov',
+  author_email = 'dimolarov@hotmail.com',
+  url = 'https://github.com/tech4242/python-csv-to-vcard', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['csv', 'vcard', 'export'],
+  classifiers = [],
+)
