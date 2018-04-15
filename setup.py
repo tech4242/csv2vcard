13ce27a93f78151e2bd3939584c5b3b129d6bddb
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-04-15 18:30:59 +0200

    tweaks for pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
+import os
 from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
 setup(
   name = 'csv2vcard',
-  packages = ['csv2vcard'], # this must be the same as the name above
+  packages = ['csv2vcard'],
   version = '0.1',
   description = 'A library for converting csvs to vCards',
+  long_description = README,
   author = 'Nikolay Dimolarov',
   author_email = 'dimolarov@hotmail.com',
-  url = 'https://github.com/tech4242/csv2vcard', # use the URL to the github repo
-  download_url = 'https://github.com/tech4242/csv2vcard/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/tech4242/csv2vcard',
+  download_url = 'https://github.com/tech4242/csv2vcard/archive/0.1.tar.gz',
   keywords = ['csv', 'vcard', 'export'],
-  classifiers = [],
+  python_requires = '>=3.5',
+  classifiers = [
+    'Development Status :: 3 - Alpha','License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    ],
 )
