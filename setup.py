56cc864e9aa35229dd2b9e1654ded269cf3226c1
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-04-24 08:59:07 +0200

    changed readme

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 setup(
   name = 'csv2vcard',
   packages = ['csv2vcard'],
-  version = '0.2.1',
+  version = '0.2.2',
   description = 'A library for converting csvs to vCards',
   long_description = README,
+  long_description_content_type='text/markdown',
   author = 'Nikolay Dimolarov',
   author_email = 'dimolarov@hotmail.com',
   url = 'https://github.com/tech4242/csv2vcard',
-  download_url = 'https://github.com/tech4242/csv2vcard/archive/0.2.1.tar.gz',
+  download_url = 'https://github.com/tech4242/csv2vcard/archive/0.2.2.tar.gz',
   keywords = ['csv', 'vcard', 'export'],
   python_requires = '>=3.6',
   classifiers = [
