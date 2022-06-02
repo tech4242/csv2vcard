5d9f7f0efc00f7788cb9527d6bc0ddefa73f68fc
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2022-06-02 09:55:56 -0400

    Make Columns Optional
    Use get with a default value of "" to prevent key errors if columns are missing.

--- csv2vcard/create_vcard.py
+++ csv2vcard/create_vcard.py
@@ -4,22 +4,22 @@ def create_vcard(contact: dict):
     """
     vc_begin = "BEGIN:VCARD\n"
     vc_version = "VERSION:3.0\n"
-    vc_name = f"N;CHARSET=UTF-8:{contact['last_name']};{contact['first_name']};;;\n"
-    vc_title = f"TITLE;CHARSET=UTF-8:{contact['title']}\n"
-    vc_org = f"ORG;CHARSET=UTF-8:{contact['org']}\n"
-    vc_phone = f"TEL;TYPE=WORK,VOICE:{contact['phone']}\n"
-    vc_email = f"EMAIL;TYPE=WORK:{contact['email']}\n"
-    vc_website = f"URL;TYPE=WORK:{contact['website']}\n"
-    vc_address = f"ADR;TYPE=WORK;CHARSET=UTF-8:{contact['street']};{contact['city']};{contact['p_code']};{contact['country']}\n"
+    vc_name = f"N;CHARSET=UTF-8:{contact.get('last_name', '')};{contact.get('first_name', '')};;;\n"
+    vc_title = f"TITLE;CHARSET=UTF-8:{contact.get('title', '')}\n"
+    vc_org = f"ORG;CHARSET=UTF-8:{contact.get('org', '')}\n"
+    vc_phone = f"TEL;TYPE=WORK,VOICE:{contact.get('phone', '')}\n"
+    vc_email = f"EMAIL;TYPE=WORK:{contact.get('email', '')}\n"
+    vc_website = f"URL;TYPE=WORK:{contact.get('website', '')}\n"
+    vc_address = f"ADR;TYPE=WORK;CHARSET=UTF-8:{contact.get('street', '')};{contact.get('city', '')};{contact.get('p_code', '')};{contact.get('country', '')}\n"
     vc_end = "END:VCARD\n"
 
-    vc_filename = f"{contact['last_name'].lower()}_{contact['first_name'].lower()}.vcf"
+    vc_filename = f"{contact.get('last_name', '').lower()}_{contact.get('first_name', '').lower()}.vcf"
     vc_output = vc_begin + vc_version + vc_name + vc_title + vc_org + vc_phone + vc_email + vc_website + vc_address + vc_end
 
     vc_final = {
         "filename" : vc_filename,
         "output" : vc_output,
-        "name" : contact['first_name'] + contact['last_name'],
+        "name" : contact.get('first_name', '') + contact.get('last_name', ''),
     }
 
     return vc_final
